4ac1c4c17d689b8455523030003c727baf07b20d
whitekkk <white_itsme@hotmail.com>
2016-11-06 19:08:40 +0700

    Beta 1.2.3

--- src/components/ExplanLawInGame.vue
+++ src/components/ExplanLawInGame.vue
@@ -1,12 +1,14 @@
 <template>
-  <div>
-    <h3 style="position: absolute; left: 5px; bottom: -15px; color: #000000;">
-          Please read <br>
+  <div style="position: absolute; left: 5px; bottom: -15px; color: #000000;">
+    <h3>
+      <center>
+          Please Read <br>
+      </center>
           Z = run <br>
           X = eat <br>
           Eat rice = 2 point <br>
           ** Warning ** <br>
-          You can eat color rice when your score more than 5
+          Color Rice for score more than 5
         </h3>
   </div>
 </template>
